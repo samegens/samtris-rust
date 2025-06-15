54d20f6155f16c19771f997d9178d824b9b57d32
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-06-15 16:36:42 +0200

    Fixed: formatting.

--- src/display.rs
+++ src/display.rs
@@ -6,7 +6,11 @@ pub trait Display {
     type Error;
 
     fn clear(&mut self) -> Result<(), Self::Error>;
-    fn draw_block(&mut self, position: Position, tetromino_type: TetrominoType) -> Result<(), Self::Error>;
+    fn draw_block(
+        &mut self,
+        position: Position,
+        tetromino_type: TetrominoType,
+    ) -> Result<(), Self::Error>;
     fn draw_playfield_border(&mut self) -> Result<(), Self::Error>;
     fn present(&mut self) -> Result<(), Self::Error>;
 }
