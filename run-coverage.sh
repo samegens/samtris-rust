d77a680ace657ee05327feed8d042d1087c613a7
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-06-15 07:56:48 +0200

    Fixed: tarpauling error.

--- run-coverage.sh
+++ run-coverage.sh
@@ -2,4 +2,10 @@
 
 set -euo pipefail
 
-cargo tarpaulin --out Html
+if ! command -v cargo-tarpaulin &> /dev/null; then
+    cargo install cargo-tarpaulin
+else
+    echo "cargo-tarpaulin already installed"
+fi
+
+cargo tarpaulin --verbose --all-features --workspace --timeout 120 --out Xml --out Html
