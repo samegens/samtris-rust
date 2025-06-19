008ab9b28f02208b9427007e1af52b92a8947d9c
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-06-19 09:39:00 +0200

    Changed: not trying to cover main and sdl_display.

--- run-coverage.sh
+++ run-coverage.sh
@@ -8,4 +8,11 @@ else
     echo "cargo-tarpaulin already installed"
 fi
 
-cargo tarpaulin --verbose --all-features --workspace --timeout 120 --out Xml --out Html
+cargo tarpaulin \
+  --exclude-files src/graphics/sdl_display.rs src/main.rs \
+  --verbose \
+  --all-features \
+  --workspace \
+  --timeout 120 \
+  --out Xml \
+  --out Html
