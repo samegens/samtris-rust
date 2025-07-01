43d257dfb45a4e19f42e886ce038f4cc6740647e
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-07-01 06:54:11 +0200

    Changed: can now use proper name for high_scores.rs within module high_scores.

--- run-lint.sh
+++ run-lint.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 cargo fmt --all -- --check
-cargo clippy --all-targets --all-features -- -D warnings -A dead_code
+cargo clippy --all-targets --all-features -- -D warnings -A dead_code -A clippy::module-inception
